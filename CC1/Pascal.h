b78772399db74496681b256a5db740108c532154
Chase Brown <cbrown54@student06.cse.nd.edu>
2020-08-22 20:09:29 -0400

    created files

---
+++ CC1/Pascal.h
@@ -0,0 +1,18 @@
+#ifndef LAB1_H
+#define LAB1_H
+
+#include <iostream> /*C++ version of stdio.h*/
+#include <cstdlib> /* Equivalent of stdlib.h */
+
+#define COUT std::cout
+#define ENDL std::endl
+#define CIN std::cin
+#define OST std::ostream //this was for trying to setup the friend in class for Lab1c
+//test
+int getTriangleSize();
+
+int * buildTriangle(int rows);
+
+int getLocation(int row, int col);
+
+#endif
