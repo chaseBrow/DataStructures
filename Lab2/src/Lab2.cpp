507e3fe8b26501d0fe24388b043602cc1ba88117
Chase Brown <cbrown54@student06.cse.nd.edu>
2020-08-26 19:06:36 -0400

    all done

--- Lab2/src/Lab2.cpp
+++ Lab2/src/Lab2.cpp
@@ -1,38 +1,59 @@
 #include "../include/functions.h"
 
-template<class Type>
-void printElem(VECT<Type> theVect) {
-    for(unsigned int i = 0; i < theVect.size(); i++) {
-        COUT << theVect.at(i) << ENDL;
-    }
-}
-
 int main() {
+    COUT << "---------------------------------------------------------" << ENDL;
+    COUT << "First Test: Empty Array" << ENDL;
+    VECT<int> empty;
+    printElem(empty);
+    COUT << "Result: " << lngIncSeq(empty) << ENDL;
+
+    COUT << "---------------------------------------------------------" << ENDL;
+    COUT << "Second Test: Negative and Positive Integers" << ENDL;
     VECT<int> ints;
-    int a[] = {10, 9, 2, 5, 3, 7, 101, 18};
+    int a[] = {10, -9, 2, 5, 3, -7, 101, 18};
     ints.assign(a, a + 8);
     printElem(ints);
+    COUT << "the length is: " << lngIncSeq(ints) << ENDL;
 
+
+    COUT << "---------------------------------------------------------" << ENDL;
+    COUT << "Third Test: Unsigned Integers" << ENDL;
     VECT<unsigned int> unsig;
     int b[] = {10, 5, 8, 3, 9, 4, 12, 11, 12};
     unsig.assign(b, b + 9);
     printElem(unsig);
+    COUT << "the length is: " << lngIncSeq(unsig) << ENDL;
 
+
+    COUT << "---------------------------------------------------------" << ENDL;
+    COUT << "Fourth Test: Characters" << ENDL;
     VECT<char> chars;
     int c[] = {'e', 'A', 'B', 'a', 'C', 'f', 'D', 'E'};
     chars.assign(c, c + 8);
     printElem(chars);
+    COUT << "the length is: " << lngIncSeq(chars) << ENDL;
 
-    VECT<int> unsig2;
-    int d[] = {10, 5, 7, 92, 14, 8, 3, 9, 1, 4, 12, 11, 27, 12};
-    unsig2.assign(d, d + 14);
-    printElem(unsig2);
+    COUT << "---------------------------------------------------------" << ENDL;
+    COUT << "Fifth Test: Random 15 Integers" << ENDL;
+    VECT<unsigned int> random;
+    for(unsigned int i = 0; i < 15; i++) {
+        random.push_back(rand() % 250);
+    }
+    printElem(random);
+    COUT << "the length is: " << lngIncSeq(random) << ENDL;
 
+    COUT << "---------------------------------------------------------" << ENDL;
+    COUT << "Sixth Test: Strings" << ENDL;
     VECT<STR> strings;
-    int e[] = {"Hello", "my", "name", "is", "is Chase", "Chase", "Chase Brown"};
-    strings.assign(e, e + 7);
+    strings.push_back("Hello,");
+    strings.push_back("my,");
+    strings.push_back("name,");
+    strings.push_back("is,");
+    strings.push_back("is Chase,");
+    strings.push_back("Chase,");
+    strings.push_back("Chase Brown,");
     printElem(strings);
-
+    COUT << "the length is: " << lngIncSeq(strings) << ENDL;
 
     return 0;
 }
