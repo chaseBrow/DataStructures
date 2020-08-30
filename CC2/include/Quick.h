a51096a6159a02850674f980d761b551737834c4
Chase Brown <rappybrown@gmail.com>
2020-08-30 15:33:08 -0400

    segmentation fault

--- CC2/include/Quick.h
+++ CC2/include/Quick.h
@@ -17,34 +17,33 @@
 #define RAND std::random_shuffle
 #define SWAP std::swap
 
-
-//this function is used for printing out the array
-template<class Type>
-int partition(VECT<Type> & theVect, int left, int right) {
-    int low = theVect[left];
-    left++;
-    while(left <= right) {
-        while(theVect[left] < theVect[low]) {
-            left++;
-        }
-        while(theVect[right] > theVect[low]) {
-            right--;
-        }
-        if(left <= right) {
-            SWAP(theVect[left], theVect[right]);
-            left++;
-            right++;
-        }
-    }
-    return left;
-}
-
 template<class Type>
 void print(VECT<Type> & theVect) {
     for (int i = 0; i < theVect.size(); i++) {
         COUT << theVect.at(i) << ", ";
     }
     COUT << ENDL;
+}
+
+template<class Type>
+int partition(VECT<Type> & theVect, int left, int right) {
+    int low = left;
+    left++;
+    while(left <= right) {
+        while(theVect.at(left) < theVect.at(low)) {
+            left++;
+        }
+        while(theVect.at(right) > theVect.at(low)) {
+            right--;
+        }
+        if(left <= right) {
+            SWAP(theVect.at(left), theVect.at(right));
+            left++;
+            right++;
+        }
+    }
+    print(theVect);
+    return left;
 }
 
 template<class Type>
