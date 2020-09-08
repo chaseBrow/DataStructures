0d2f783bc53b27647d80a29bc5d31ef1d31899a2
Chase Brown <rappybrown@gmail.com>
2020-09-08 15:24:15 -0400

    finished

--- Lab4/include/MergeInt.h
+++ Lab4/include/MergeInt.h
@@ -13,7 +13,7 @@
 #define VECT std::vector
 #define SORT std::sort
 
-void printVect(VECT< VECT<int> >& printMe) {
+void printVect(VECT< VECT<int> > printMe) {
     for(long unsigned int j = 0; j < printMe.size(); j++) {
         COUT << "{";
         for(long unsigned int i = 0; i < printMe.at(j).size(); i++) {
@@ -25,9 +25,45 @@ void printVect(VECT< VECT<int> >& printMe) {
     COUT << ENDL;
 }
 
+bool cmp(VECT<int> a, VECT<int> b) {
+    return a[0]<b[0];
+}
+
+int max(int& val1, int& val2) {
+    COUT << "test4" << ENDL;
+    return (val1 > val2) ? val1 : val2;
+}
+
+int min(int& val1, int& val2) {
+    COUT << "test5" << ENDL;
+    return (val1 < val2) ? val1 : val2;
+}
+
 void merge(VECT< VECT<int> >& intervals) {
-    VECT<int> test = {10,20};
-    intervals.push_back(test);
+    SORT(intervals.begin(), intervals.end(), cmp);
+    printVect(intervals);
+
+    int pos = 0;
+
+    for(long unsigned int i = 1; i < intervals.size(); i++) {
+        COUT << "test1" << ENDL;
+        if(intervals.at(pos).at(1) >= intervals.at(i).at(0)) {
+            COUT << "test2" << ENDL;
+            intervals.at(pos).at(1) = max(intervals.at(pos).at(1), intervals.at(i).at(1));
+            intervals.at(pos).at(0) = min(intervals.at(pos).at(0), intervals.at(i).at(0));
+            COUT << "mid: ";
+            printVect(intervals);
+            intervals.erase(intervals.begin() + 1);
+            COUT << "midPost: ";
+            printVect(intervals);
+            pos++;
+        }
+        else {
+            COUT << "test3" << ENDL;
+            pos++;
+            intervals.at(pos) = intervals.at(i);
+        }
+    }
     printVect(intervals);
 }
 
--- Lab4/src/Lab4.cpp
+++ Lab4/src/Lab4.cpp
@@ -4,7 +4,6 @@
  int main() {
      COUT << "--------------------------------------------------------" << ENDL;
      COUT << "Test 1: " << ENDL;
-     VECT< VECT<int> > intervals = {{1,3},{2,6},{8,10},{15,18}};
+     VECT< VECT<int> > intervals = {{2,6},{8,10},{1,3},{15,18}};
      merge(intervals);
-     printVect(intervals);
-}
+ }
