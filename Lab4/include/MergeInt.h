2bd382c294f13fecbd592b4ccd05b9c11b88d280
Chase Brown <rappybrown@gmail.com>
2020-09-08 16:00:35 -0400

    fixed containing

--- Lab4/include/MergeInt.h
+++ Lab4/include/MergeInt.h
@@ -30,40 +30,33 @@ bool cmp(VECT<int> a, VECT<int> b) {
 }
 
 int max(int& val1, int& val2) {
-    COUT << "test4" << ENDL;
     return (val1 > val2) ? val1 : val2;
 }
 
 int min(int& val1, int& val2) {
-    COUT << "test5" << ENDL;
     return (val1 < val2) ? val1 : val2;
 }
 
 void merge(VECT< VECT<int> >& intervals) {
     SORT(intervals.begin(), intervals.end(), cmp);
+    COUT << "before: ";
     printVect(intervals);
 
     int pos = 0;
 
     for(long unsigned int i = 1; i < intervals.size(); i++) {
-        COUT << "test1" << ENDL;
         if(intervals.at(pos).at(1) >= intervals.at(i).at(0)) {
-            COUT << "test2" << ENDL;
             intervals.at(pos).at(1) = max(intervals.at(pos).at(1), intervals.at(i).at(1));
             intervals.at(pos).at(0) = min(intervals.at(pos).at(0), intervals.at(i).at(0));
-            COUT << "mid: ";
-            printVect(intervals);
-            intervals.erase(intervals.begin() + 1);
-            COUT << "midPost: ";
-            printVect(intervals);
-            pos++;
+            intervals.erase(intervals.begin() + i);
+            i--;
         }
         else {
-            COUT << "test3" << ENDL;
             pos++;
             intervals.at(pos) = intervals.at(i);
         }
     }
+    COUT << "after: ";
     printVect(intervals);
 }
 
--- Lab4/src/Lab4.cpp
+++ Lab4/src/Lab4.cpp
@@ -3,7 +3,27 @@
 #include "../include/MergeInt.h"
  int main() {
      COUT << "--------------------------------------------------------" << ENDL;
-     COUT << "Test 1: " << ENDL;
-     VECT< VECT<int> > intervals = {{2,6},{8,10},{1,3},{15,18}};
+     COUT << "Test 1: Example 1" << ENDL;
+     VECT< VECT<int> > intervals = {{1,3},{2,6},{8,10},{15,18}};
+     merge(intervals);
+
+     COUT << "--------------------------------------------------------" << ENDL;
+     COUT << "Test 2: sharing an end value with the start" << ENDL;
+     intervals = {{1,4},{4,5}};
+     merge(intervals);
+
+     COUT << "--------------------------------------------------------" << ENDL;
+     COUT << "Test 3: Empty vectors" << ENDL;
+     intervals = {};
+     merge(intervals);
+
+     COUT << "--------------------------------------------------------" << ENDL;
+     COUT << "Test 4: unsorted" << ENDL;
+     intervals = {{16,24},{2,6},{8,10},{1,3},{15,18}};
+     merge(intervals);
+
+     COUT << "--------------------------------------------------------" << ENDL;
+     COUT << "Test 4: values contained in bounds of previous" << ENDL;
+     intervals = {{1,3},{2,6},{3,5},{8,10},{15,18}};
      merge(intervals);
  }
