b3608d58ab93d52bc45750c0515d4a793847ee6d
Chase Brown <rappybrown@gmail.com>
2020-09-08 13:48:45 -0400

    initiated

---
+++ Lab4/include/MergeInt.h
@@ -0,0 +1,17 @@
+//Author: Chase Brown
+//Email: cbrown54@nd.edu
+
+#ifndef QUICK_H
+#define QUICK_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#define COUT std::cout
+#define ENDL std::endl
+#define STR std::string
+
+
+#endif
